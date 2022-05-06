dfdb433e1d092bfcf01b542f25d18ef38d845b5c
ltoker <lilchpilch@yahoo.com>
2022-05-06 16:39:06 +0200

    Clean and add CIBERSORT and dtangle

--- ProjectScripts/MainAnalysis.R
+++ ProjectScripts/MainAnalysis.R
@@ -15,15 +15,26 @@ if(!ResultsPath %in% list.dirs(full.names = F, recursive = F)){
 }
 ResultsPath = paste0(ResultsPath, "/")
 
-AllData <- readRDS("Data/AllEstimatesAndMeta.Rds")
-#IHC_Microglia <- read.table("Data/Microglia_IHC.txt", header = T, sep = "\t")
+# AllData <- readRDS("Data/AllEstimatesAndMeta.Rds")
+# AllData %<>% mutate(across(contains("MGP"), rescale, c(0,1)))
 
-AllData %<>% mutate(across(contains("MGP"), rescale, c(0,1)))
+BS_H3K27acEstimates <- readRDS("Data/BS_H3K27acEstimates.Rds")
 
-SuppTable2 <- AllData %>% select(BioBankID, Cohort, Condition, Age, Sex, PMI, RIN, Resequenced)
+RNAestimates_MGP_Bisque <- read.table("EstimatesResults/RNAestimatesMGP_Bisque.tsv",
+                                      header = T, sep = "\t")
+
+
+AllData <- merge(RNAestimates_MGP_Bisque, BS_H3K27acEstimates,
+                 by = "Biobank_ID")
+
+
+names(AllData) <- sapply(names(AllData), function(x){
+  gsub("s$", "", x)
+})
+
+SuppTable2 <- AllData %>% select(Biobank_ID, Cohort, Group, Age, Sex, PMI, RIN, Resequenced)
 write.table(SuppTable2, paste0(ResultsPath, "SuppTable2.tsv"), sep = "\t", row.names = F, col.names = T)
 
-#AllData <- merge(AllData, IHC_Microglia, by = "BioBankID", all.x = T, sort = F )
 
 AllData_long <- pivot_longer(AllData, cols = matches("^T._"), names_sep = "_",
                              names_to = c("Tissue", "Matherial", "Method", "CellType"), values_to = "Estimate")
@@ -32,6 +43,143 @@ AllData_long <- pivot_longer(AllData, cols = matches("^T._"), names_sep = "_",
 
 CellTypes <- unique(AllData_long$CellType)
 
+BrainDecode <- sapply(list.files("Data", pattern = "CIBERSORT|dtangle"), function(File){
+  temp <- read.table(paste0("Data/", File), sep = "\t", header = T)
+  Method = strsplit(File, "_")[[1]][1]
+  if(grepl("RNAseq1", File)){
+    IDcol = "RNAseq_id_ParkOme1"
+    Pref  = paste0("T1_RNA_", Method, "_")
+  } else if(grepl("RNAseq2", File)){
+    IDcol = "RNAseq_id_ParkOme2"
+    Pref  = paste0("T2_RNA_", Method, "_")
+  }
+  rownames(temp) <- temp[,1]
+  temp <- temp[,-1]
+  names(temp) <- paste0(Pref, names(temp))
+  
+  temp$Biobank_ID <- AllData$Biobank_ID[match(rownames(temp), AllData[[IDcol]])]
+  temp
+}, simplify = F)
+
+AllRNAestimates <- RNAestimates_MGP_Bisque
+
+for(Val in names(BrainDecode)){
+  AllRNAestimates <- merge(AllRNAestimates, BrainDecode[[Val]], by = "Biobank_ID")
+}
+
+AllRNAestimateslong <- pivot_longer(AllRNAestimates, cols = matches("^T._"), names_sep = "_",
+                                    names_to = c("Tissue", "Matherial", "Method", "CellType"), values_to = "Estimate")
+AllRNAestimateslong$Method <- factor(AllRNAestimateslong$Method, levels = c("Bisque", "dtangle", "CIBERSORT", "MGP"))
+AllRNAestimateslong$CellType <- factor(AllRNAestimateslong$CellType, levels = unique(AllRNAestimateslong$CellType))
+
+AllRNAestimateslong$Dataset <- sapply(AllRNAestimateslong$Tissue, function(x){
+  if(x == "T1"){
+    "RNAseq1"
+  } else {
+    "RNAseq2"
+  }
+})
+
+######## Comparisson to IHC data and all method correlations #############
+
+#Get IHC quantification of cortical samples from Patrick et el. 2020
+IHC_Patrick <- sapply(c("astro", "endo", "microglia", "oligo", "neuro"), function(CellType){
+  temp <- read.table(paste0("https://github.com/ellispatrick/CortexCellDeconv/raw/master/CellTypeDeconvAnalysis/Data/IHC.",
+                                    CellType, ".txt"), sep = "\t", header = T) %>% t %>% data.frame()
+  names(temp)[1] <- "Estimate"
+  CellType <- if(CellType == "astro"){
+    "Astrocyte"
+  } else if (CellType == "endo"){
+    "Endothelial"
+  } else if (CellType == "microglia"){
+    "Microglia"
+  }
+  else if (CellType == "oligo"){
+    "Oligodendrocyte"
+  }
+  else if (CellType == "neuro"){
+    "Neuron"
+  }
+  temp %>% mutate(SubjectID = rownames(.), CellType = CellType, Dataset = "Patrick_et.al", Method = "IHC" )
+}, simplify = F) %>% rbindlist() %>% data.frame()
+
+#Plotting comparison between IHC-based estimates from Patrick et al 2020 and across-cell based
+#estimation approaches
+IHC_RNA <- rbind(AllRNAestimateslong %>% filter(Method != "MGP") %>% mutate(RNAdataset = Dataset) %>%
+                   select(Estimate, CellType, Dataset, RNAdataset, Method),
+                 IHC_Patrick %>% select(-SubjectID) %>% mutate(RNAdataset = "RNAseq1"),
+                 IHC_Patrick %>% select(-SubjectID) %>% mutate(RNAdataset = "RNAseq2"),
+                 data.frame(Estimate = 0.5,  CellType = CellTypes, Dataset = "temp",  RNAdataset = "RNAseq1", Method = "bla"),
+                 data.frame(Estimate = 0.5,  CellType = CellTypes, Dataset = "temp", RNAdataset = "RNAseq2",  Method = "bla")) %>% droplevels()
+
+IHC_RNA$Method <- factor(IHC_RNA$Method, levels = levels(IHC_RNA$Method)[c(1:3, 5, 4)])
+
+ggplot(IHC_RNA, aes(CellType, Estimate, fill = Method, color = Method)) +
+  theme_bw() +
+  labs(x = "", y = "Estimate (proportion of cells)") +
+  geom_boxplot(outlier.shape = NA, alpha = 0.6, color = "black") +
+  scale_fill_manual(values = c("#831F02", MoviePalettes$SpiritedAway[c(4,9)], "green", "black")) +
+  scale_color_manual(values = c("#831F02", MoviePalettes$SpiritedAway[c(4,9)], "green", "black")) +
+  geom_point(position=position_jitterdodge(jitter.width = 0.3), show.legend = F, size = 0.3) +
+  geom_vline(xintercept = c(1.5, 2.5, 3.5, 4.5), lty = "dashed") + 
+  facet_wrap(~RNAdataset, nrow = 2) 
+ggsave(paste0(ResultsPath, "RNA_IHC_estimates.pdf"), device = "pdf", width = 10, height = 5, useDingbats = F)
+
+ggplot(IHC_RNA %>% filter(Dataset != "temp"), aes(Method , Estimate, fill = Method, color = Method)) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust =  1)) +
+  labs(x = "", y = "Estimate (proportion of cells)") +
+  geom_boxplot(outlier.shape = NA, alpha = 0.6, color = "black") +
+  scale_fill_manual(values = c("#831F02", MoviePalettes$SpiritedAway[c(4,9)], "black")) +
+  scale_color_manual(values = c("#831F02", MoviePalettes$SpiritedAway[c(4,9)], "black")) +
+  geom_point(position=position_jitterdodge(jitter.width = 1), size = 0.3, show.legend = F) +
+  facet_grid(CellType~Dataset, scales = "free_x", space = "free")
+ggsave(paste0(ResultsPath, "RNA_IHC_estimates2.pdf"), device = "pdf", width = 7, height = 7, useDingbats = F)
+
+AllRNAcomparison <- sapply(CellTypes, function(CellType){
+  sapply(c("T1", "T2"), function(TS){
+     Data <- AllRNAestimates %>% select(matches(CellType)) %>%
+       select(matches(TS))
+     cor(Data)
+  }, simplify = F)
+ 
+}, simplify = F)
+
+AllRNAcomparisonDF <- sapply(names(AllRNAcomparison), function(CellType){
+  temp <- AllRNAcomparison[[CellType]]
+  sapply(names(temp), function(TS){
+    temp2 <- temp[[TS]] %>% data.frame()
+    diag(temp2) <- NA
+    names(temp2) <- sapply(names(temp2), function(x){
+      strsplit(x, "_")[[1]][[3]]
+    })
+    temp2 %<>%  mutate(CellType = CellType, Tissue = TS, Dataset = TS)
+    temp2$Dataset <- sapply(temp2$Dataset, function(x){
+      gsub("T", "RNAseq", x)
+    })
+    temp2
+  }, simplify = F) %>% rbindlist() %>% data.frame()
+}, simplify = F) %>% rbindlist() %>% data.frame() %>%
+  pivot_longer(cols = c(1:4), names_to = "Method", values_to = "Correlation") %>% data.frame()
+
+AllRNAcomparisonDF$CellType <- factor(AllRNAcomparisonDF$CellType, levels = unique(AllRNAcomparisonDF$CellType) )
+AllRNAcomparisonDF$Method <- factor(AllRNAcomparisonDF$Method, levels = c("Bisque", "dtangle",
+                                                                          "CIBERSORT", "MGP"))
+
+ggplot(AllRNAcomparisonDF, aes(CellType, Correlation, color = Method, fill = Method)) +
+  theme_bw() +
+  theme(panel.grid.major.x = element_blank()) +
+  labs(x = "") +
+  geom_boxplot(outlier.shape = NA, alpha = 0.6, color = "black") +
+  geom_point(position=position_jitterdodge(jitter.width = 0.08), show.legend = F) +
+  scale_color_manual(values = c("#831F02", MoviePalettes$SpiritedAway[c(4,9)], "#339656")) +
+  scale_fill_manual(values = c("#831F02", MoviePalettes$SpiritedAway[c(4,9)], "#339656")) +
+  geom_vline(xintercept = c(1.5, 2.5, 3.5, 4.5), lty = "dashed")
+ggsave(paste0(ResultsPath, "AllRNA_estimatesCor.pdf"), device = "pdf", width = 10, height = 3, useDingbats = F)
+
+  
+##################################################################################################
+
 RNA_MethodComparison <-   sapply(CellTypes, function(CellType){
   T1 = cor.test(formula(paste0("~T1_RNA_MGP_", CellType, "+T1_RNA_Bisque_", CellType)), data = AllData)
   T2 = cor.test(formula(paste0("~T2_RNA_MGP_", CellType, "+T2_RNA_Bisque_", CellType)), data = AllData %>% filter(Resequenced == "No"))
@@ -39,7 +187,7 @@ RNA_MethodComparison <-   sapply(CellTypes, function(CellType){
 }, simplify = F) %>% rbindlist() %>% data.frame() %>% pivot_longer(-CellType,
                                                                    names_to = "TissueSample",
                                                                    values_to = "Cor") %>% data.frame()
-cor.test(AllData$T1_BS_MSP_Neurons, AllData$T1_BS_CETS_Neurons)
+cor.test(AllData$T1_BS_MSP_Neuron, AllData$T1_BS_CETS_Neuron)
 
 
 
